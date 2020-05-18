5527dee74e058582eaa6729e13049a0812b5cd0f
acharyarushikesh-hspl <rushikesh.acharya@harbingergroup.com>
2020-05-19 00:35:39 +0530

    chore: Update configurations

--- truffle.js
+++ truffle.js
@@ -1,14 +1,16 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "";
+var mnemonic = "coral edit walk trade expect inmate enroll collect express jeans appear family";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 8000000,
+      gasLimit: 8000000,
+
     }
   },
   compilers: {
